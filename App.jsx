6f35f68b47c3761a65fbdf65e2b8178fe075aabe
AnaLuizaMenezes <86384549+AnaLuizaMenezes@users.noreply.github.com>
2023-05-29 13:41:33 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import Carrossel from './components/Carrossel'
+import NavProjetos from './components/NavProjetos'
+import Cards from './components/Cards'
+import Cabeçalho from './components/Cabeçalho'
+import Rodape from './components/Rodape'
+
+
+
+
+function App() {
+  return (
+    <div>
+        <NavProjetos/>
+        <Carrossel/>
+        <Cards/>
+        <Cabeçalho/>
+        <Rodape/>
+        
+    </div>
+  )
+  }
+ 
+
+export default App
